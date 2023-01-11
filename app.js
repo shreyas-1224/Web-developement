6c921524012325b64e4a49f3e88e5178b628968a
shreyas <pshreyas1224@gmail.com>
2023-01-11 17:57:36 +0530

    just added some border to home page and removed author

--- app.js
+++ app.js
@@ -2,8 +2,10 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const _ = require("lodash");
-const app = express();
+const mongoose = require("mongoose");
 
+
+const app = express();
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
